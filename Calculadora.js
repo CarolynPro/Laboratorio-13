f17bebba50acc78e696c4e1975a6046b0a621f8b
CarolynPro <carol.ingabil.18@gmail.com>
2025-03-17 20:33:38 -0500

    Agregar codigo

---
+++ Calculadora.js
@@ -0,0 +1,31 @@
+let pantalla = document.getElementById("pantalla");
+let operacion = "";
+
+function agregarNumero(numero){
+    operacion += numero;
+}
+function agregarOperador(){
+    if( operacion !== "" && !/[\+\-\*\/]$/.test(operacion)){
+    operacion += agregarOperador;
+    actualizarPantalla();
+}
+}
+function calcularResultado(){
+    try{
+        operacion = eval(operacion).toString();
+    } catch (error) {
+        operacion = "error"
+    }
+    actualizarPantalla(); 
+          
+}
+function limpiarPantalla(){
+    operacion =""
+    actualizarPantalla(); 
+}
+
+
+function actualizarPantalla(){
+    pantalla.value = operacion;
+}
+
